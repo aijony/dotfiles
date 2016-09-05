5a056c8f8f1bebce089706c7604b7fc833174a44
Aidan Nyquist <contact@aidannyquist.com>
2016-09-05 12:46:02 -0700

    Add scripts folder

---
+++ .scripts/ycmd/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+cd ~/
+
+mkdir .ycmd
+
+cd .ycmd
+
+git clone https://github.com/rdnetto/YCM-Generator
+
+git clone https://github.com/Valloric/ycmd
+
+cd ycmd
+
+git submodule update --init --recursive
+
+./build.py --all
+
+cd ~/.emacs.d
+
+mkdir private
+
+cd private
+
+svn checkout https://github.com/aijony/dotfiles/trunk/.emacs.d/private/better-auto-completion
