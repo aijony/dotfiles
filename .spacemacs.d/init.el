7d2ee96aa95496bb66db2241965dff9dc14e8976
aijony <aijony@users.noreply.github.com>
2017-02-05 15:58:27 -0800

    Clean up commit

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -356,47 +356,6 @@ you should place your code here."
 
 
   (load-theme 'aidan-dark)
-
-  ;;Set company behavior
-  (let ((map company-active-map))
-    (define-key map (kbd "<tab>") 'company-complete-selection)
-    (define-key map (kbd "RET") 'nil)
-    )
-
-
-  (eval-after-load 'semantic
-    (add-hook 'semantic-mode-hook
-              (lambda ()
-                (dolist (x (default-value 'completion-at-point-functions))
-                  (when (string-prefix-p "semantic-" (symbol-name x))
-                    (remove-hook 'completion-at-point-functions x))))))
-
-  (setq flycheck-check-syntax-automatically '(mode-enabled new-line)
-
-
-  (defun my-insert-file-name (filename &optional args)
-    "Insert name of file FILENAME into buffer after point.
-
-  Prefixed with \\[universal-argument], expand the file name to
-  its fully canocalized path.  See `expand-file-name'.
-
-  Prefixed with \\[negative-argument], use relative path to file
-  name from current directory, `default-directory'.  See
-  `file-relative-name'.
-
-  The default with no prefix is to insert the file name exactly as
-  it appears in the minibuffer prompt."
-    ;; Based on insert-file in Emacs -- ashawley 20080926
-    (interactive "*fInsert file name: \nP")
-    (cond ((eq '- args)
-           (insert (file-relative-name filename)))
-          ((not (null args))
-           (insert (expand-file-name filename)))
-          (t
-           (insert filename))))
-
-  (evil-leader/set-key "f i" 'my-insert-file-name)
-
   (setq powerline-default-separator 'arrow)
   (setq-default dotspacemacs-line-numbers 'relative)
   ;;Hides python error message occurring in org snippets
@@ -415,17 +374,6 @@ you should place your code here."
 
 
   )
-
-  ;;Thanks to git hub user Paulo Costa @pcesar22 All calls to
-  ;;perl-mode now use cperl-mode
-  (defalias 'perl-mode 'cperl-mode)
-  ;;Cperl mode hook enables company auto complete and fixes issue
-  ;;with cperl-electric and smart-parens
-  (add-hook 'cperl-mode-hook
-            #'company-mode
-            (lambda () (local-unset-key (kbd "{")))
-            )
-
   )
 
 
--- .spacemacs.d/private/better-auto-completion/config.el
+++ .spacemacs.d/private/better-auto-completion/config.el
@@ -14,6 +14,38 @@
 ;;Build directory settings
 
 
+;;Set company behavior
+(let ((map company-active-map))
+  (define-key map (kbd "<tab>") 'company-complete-selection)
+  (define-key map (kbd "RET") 'nil)
+  )
+
+
+;;Prevent parsing hold-up
+(eval-after-load 'semantic
+  (add-hook 'semantic-mode-hook
+            (lambda ()
+              (dolist (x (default-value 'completion-at-point-functions))
+                (when (string-prefix-p "semantic-" (symbol-name x))
+                  (remove-hook 'completion-at-point-functions x))))))
+
+
+(setq flycheck-check-syntax-automatically '(mode-enabled new-line)
+
+
+;;Thanks to git hub user Paulo Costa @pcesar22 All calls to
+;;perl-mode now use cperl-mode
+(defalias 'perl-mode 'cperl-mode)
+;;Cperl mode hook enables company auto complete and fixes issue
+;;with cperl-electric and smart-parens
+(add-hook 'cperl-mode-hook
+          #'company-mode
+          (lambda () (local-unset-key (kbd "{")))
+          )
+
+
+
+;;YCMD
 
 ;;Safe command for .dir-locals.el
 (put 'helm-make-build-dir 'safe-local-variable 'stringp)
@@ -27,4 +59,29 @@
 (set-variable 'ycmd-server-command '("python" "/usr/bin/ycmd/ycmd"))
 (set-variable 'ycmd-extra-conf-handler 'load)
 (setq ycmd-generate-command '"~/.ycmd/YCM-Generator/config_gen.py")
+
+
+;;Fuzzy-file-insert
+(defun my-insert-file-name (filename &optional args)
+  "Insert name of file FILENAME into buffer after point.
+
+  Prefixed with \\[universal-argument], expand the file name to
+  its fully canocalized path.  See `expand-file-name'.
+
+  Prefixed with \\[negative-argument], use relative path to file
+  name from current directory, `default-directory'.  See
+  `file-relative-name'.
+
+  The default with no prefix is to insert the file name exactly as
+  it appears in the minibuffer prompt."
+  ;; Based on insert-file in Emacs -- ashawley 20080926
+  (interactive "*fInsert file name: \nP")
+  (cond ((eq '- args)
+         (insert (file-relative-name filename)))
+        ((not (null args))
+         (insert (expand-file-name filename)))
+        (t
+         (insert filename))))
+
+(evil-leader/set-key "f i" 'my-insert-file-name)
 
