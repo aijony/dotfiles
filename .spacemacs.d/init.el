be2293cd1182079ecc07f5d2aa9ba3c5e25564f7
aijony <aijony@users.noreply.github.com>
2017-03-13 19:35:29 -0700

    Readd theme

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -364,7 +364,8 @@ layers (configuration.)
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you (should) place your code here."
-  
+  (load-theme 'aidan-dark)
+  (setq powerline-default-separator 'arrow)
 
   (linum-relative-global-mode)
   ;;Hides python error message occurring in org snippets
