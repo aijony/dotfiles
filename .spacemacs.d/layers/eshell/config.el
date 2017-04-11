b741a501427e1b035a10668d96e8f48025aa4224
aijony <aidannyquist+git@gmail.com>
2017-04-10 20:32:05 -0700

    Add eshell support

--- .spacemacs.d/layers/eshell/config.el
+++ .spacemacs.d/layers/eshell/config.el
@@ -1,10 +1,15 @@
 
-(eval-after-load 'eshell
-  '(defun eshell/clear ()
-    "Clear the eshell buffer."
+(with-eval-after-load 'eshell
+  ;; This is an eshell alias
+  (defun eshell/clear ()
     (let ((inhibit-read-only t))
-      (erase-buffer)
-      (eshell-send-input)))
+      (erase-buffer)))
+  ;; This is a key-command
+  (defun spacemacs/eshell-clear-keystroke ()
+    "Allow for keystrokes to invoke eshell/clear"
+    (interactive)
+    (eshell/clear)
+    (eshell-send-input))
   )
 
 
--- .spacemacs.d/layers/eshell/keybindings.el
+++ .spacemacs.d/layers/eshell/keybindings.el
@@ -1,16 +1,19 @@
 
-(spacemacs/set-leader-keys-for-major-mode 'eshell-mode
-  "h" 'spacemacs/helm-eshell-history)
+(spacemacs/set-leader-keys-for-major-mode
+  'eshell-mode "h" 'spacemacs/helm-eshell-history)
 
-(spacemacs/set-leader-keys-for-major-mode 'eshell-mode
-  "c" 'eshell/clear)
+(spacemacs/set-leader-keys-for-major-mode
+  'eshell-mode "c" 'spacemacs/eshell-clear-keystroke)
 
-(eval-after-load 'eshell
-  '(define-key eshell-mode-map (kbd "<tab>") 'helm-esh-pcomplete))
+(defun eshell/set-keys ()
+  (local-set-key (kbd "<tab>")
+                 'helm-esh-pcomplete)
+  (local-set-key (kbd "C-l")
+                 'spacemacs/eshell-clear-keystroke)
+  (evil-define-key 'insert
+    eshell-mode-map
+    (kbd "<tab>")
+    'helm-esh-pcomplete))
 
-(evil-define-key 'insert eshell-mode-map
-  (kbd "<tab>") 'helm-esh-pcomplete
-  )
 
-
-
+(add-hook 'eshell-mode-hook 'eshell/set-keys)
