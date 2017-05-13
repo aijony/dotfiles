4f1c6ff88b7183f17416dade23843911cd790173
Aidan <aidannyquist@gmail.com>
2017-05-13 10:17:34 -0700

    Create setup.sh

---
+++ .scripts/arch/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#Requires a user to be made first 
+sudo pacman -Syu
+
+sudo pacman -S sagemath svn rust cargo cscope cmake haskell-stack jupyter python3 python2 neovim git lilypond go make clang base-devel ncurses boost boost-libs mono npm nodejs
+
+cd ~/.scripts/ycmd
+./install.sh
+
+stack install apply-refact hlint stylish-haskell hasktags hoogle intero hindent
