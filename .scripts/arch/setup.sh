3e07f368921bb51c84a3851375e5bf2506f75896
aidan <aidannyquist@gmail.com>
2017-05-14 14:10:29 -0700

    Add aspell support

--- .scripts/arch/setup.sh
+++ .scripts/arch/setup.sh
@@ -3,7 +3,7 @@
 #Requires a user to be made first 
 sudo pacman -Syu
 
-sudo pacman -S sagemath svn rust cargo cscope cmake haskell-stack jupyter python3 python2 neovim git lilypond go make clang base-devel ncurses boost boost-libs mono npm nodejs
+sudo pacman -S sagemath svn rust cargo cscope cmake haskell-stack jupyter python3 python2 neovim git lilypond go make clang base-devel ncurses boost boost-libs mono npm nodejs aspell
 
 cd ~/.scripts/ycmd
 ./install.sh
