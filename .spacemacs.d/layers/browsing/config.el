4b600238a65bd5f4a49542fecc58f77bb5d790b1
aidan <aidannyquist@gmail.com>
2017-05-27 20:00:34 -0700

    Fix PDF persistence

--- .spacemacs.d/layers/browsing/config.el
+++ .spacemacs.d/layers/browsing/config.el
@@ -3,16 +3,17 @@
 (setq httpd-port '8082)
 
 ;; PDF tools
-
 ;; workaround for pdf-tools not reopening to last-viewed page of the pdf:
 ;; https://github.com/politza/pdf-tools/issues/18#issuecomment-269515117
-;; TODO: save bookmarks in separate file
-
 (defun brds/pdf-set-last-viewed-bookmark ()
+  (interactive)
   (when (eq major-mode 'pdf-view-mode)
     (bookmark-set (brds/pdf-generate-bookmark-name))))
 
+
 (defun brds/pdf-jump-last-viewed-bookmark ()
+  (interactive)
+  (bookmark-set "fake")
   (when
       (brds/pdf-has-last-viewed-bookmark)
     (bookmark-jump (brds/pdf-generate-bookmark-name))))
@@ -22,8 +23,15 @@
    (brds/pdf-generate-bookmark-name) bookmark-alist))
 
 (defun brds/pdf-generate-bookmark-name ()
-  (concat "PDF-LAST-VIEWED: " (buffer-file-name)))
+  (concat "PDF-LAST-VIEWED: "  (buffer-file-name)))
+
+
+(defun brds/pdf-set-all-last-viewed-bookmarks ()
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (brds/pdf-set-last-viewed-bookmark))))
 
 (add-hook 'kill-buffer-hook 'brds/pdf-set-last-viewed-bookmark)
 (add-hook 'pdf-view-mode-hook 'brds/pdf-jump-last-viewed-bookmark)
-
+(unless noninteractive  ;; as `save-place-mode' does
+  (add-hook 'kill-emacs-hook #'brds/pdf-set-all-last-viewed-bookmarks))
