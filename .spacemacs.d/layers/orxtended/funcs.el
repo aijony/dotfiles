2c9830cf547d5f7ed9be73b523ee1c655fef5637
Aidan Nyquist <contact@aidannyquist.com>
2017-08-13 16:12:11 -0700

    Merge functions to local namespace in notmuch layer

--- .spacemacs.d/layers/orxtended/funcs.el
+++ .spacemacs.d/layers/orxtended/funcs.el
@@ -1,4 +1,3 @@
-
 (defun orxtended/transparentize ()
   (interactive)
   (if (not (string-match "ImageMagick" (shell-command-to-string "convert -version")))
@@ -23,52 +22,59 @@
 (setq org-notmuch-tag-blacklist '("inbox" "unread" "attachment"))
 (setq org-notmuch-conversion '(("flagged" . "important") ("test" . "tested")))
 (setq org-notmuch-email-link "notmuch:id:")
-(setq org-notmuch-search-link "notmuch-search")
 (setq org-notmuch-email-regexp "[=+_.~a-zA-Z0-9]*[-+_.~:a-zA-Z0-9]*@[-.a-zA-Z0-9]+")
-(setq org-notmuch-tag-regexp "[a-zA-Z0-9]")
 
 (defun org-notmuch/tag-emails ()
   (interactive)
-  ;; Get notmuch id
-  (let* ((ids (org-notmuch/get-ids org-notmuch-email-link org-notmuch-email-regexp))
-         (tags (apply 'append (mapcar 'org-notmuch/id-to-tags ids)))
-    (concat-tags (delete-dups (append (org-notmuch/check-lists tags) (org-get-tags))))
-    (temp)
-    (output-tags (dolist (tag concat-tags temp)
-                   (setq temp (concat temp tag ":")))))
-  (org-set-tags-to output-tags)))
+  (cl-flet* (
+             (check-lists (tags)
+                          (delete nil
+                                  (mapcar (lambda (tag)
+                                            (cond
+                                             ((member tag org-notmuch-tag-blacklist) 'nil)
+                                             ((not (or (member tag org-notmuch-tag-whitelist)
+                                                       (eq org-notmuch-tag-whitelist 'nil))) 'nil)
+                                             ((assoc tag org-notmuch-conversion)
+                                              (cdr (assoc tag org-notmuch-conversion)))
+                                             ('t tag)))
+                                          tags)))
 
-(defun org-notmuch/check-lists (tags)
-  (delete nil
-          (mapcar
-           (lambda (tag)
-             (cond
-              ((member tag org-notmuch-tag-blacklist) 'nil)
-              ((not (or
-                     (member tag org-notmuch-tag-whitelist)
-                     (eq org-notmuch-tag-whitelist 'nil)))
-               'nil)
-              ((assoc tag org-notmuch-conversion)
-               (cdr (assoc tag org-notmuch-conversion)))
-              ('t tag)))
-           tags)))
+             ;; Gets the notmuch ids in the org-mode section's links
+             (get-ids (regexp-link regexp-identifier)
+                      (let* ((notmuch-emails '())
+                             (pos (point))
+                             (begin (org-element-property :begin (org-element-at-point)))
+                             (end (org-element-property :end (org-element-at-point))))
+                        (goto-char begin)
+                        (while (re-search-forward regexp-link end 't)
+                          (looking-at regexp-identifier)
+                          (push (match-string 0)
+                                notmuch-emails))
+                        (goto-char pos)
+                        ;;return
+                        notmuch-emails))
 
-(defun org-notmuch/get-ids (regexp-link regexp-identifier)
-  (let* (
-         (notmuch-emails '())
-         (pos (point))
-         (begin (org-element-property :begin (org-element-at-point)))
-         (end (org-element-property :end (org-element-at-point))))
-    (goto-char begin)
-    (while (re-search-forward regexp-link end 't)
-      (looking-at regexp-identifier)
-      (push (match-string 0) notmuch-emails))
-    (goto-char pos)
-    ;;return
-    notmuch-emails))
+             ;; ":open-source:" is not a valid tag, so this converts it to "opensource"
+             (clean-tags (tags)
+                         (mapcar (lambda (tag)
+                                   (apply #'concat
+                                          (s-split-words tag)))
+                                 tags))
 
-(defun org-notmuch/id-to-tags (id)
-  (split-string (shell-command-to-string
-                 (concat "notmuch search --output:tags id:" id))))
+             ;; Converts notmuch email ids to notmuch tags
+             (ids-to-tags (ids)
+                          (apply 'append
+                                 (mapcar (lambda (id)
+                                           (split-string (shell-command-to-string
+                                                          (concat "notmuch search --output:tags id:"
+                                                                                          id))))
+                                         ids))))
 
-
+    (let* ((ids (get-ids org-notmuch-email-link org-notmuch-email-regexp))
+           (tags (ids-to-tags ids))
+           (concat-tags (delete-dups (clean-tags (append (check-lists tags)
+                                                         (org-get-tags)))))
+           (temp)
+           (output-tags (dolist (tag concat-tags temp)
+                          (setq temp (concat temp tag ":")))))
+      (org-set-tags-to output-tags))))
