cbbe9b4a25d1f17c9c8dea3de902ede44fb3a910
Aidan Nyquist <contact@aidannyquist.com>
2018-01-22 14:55:58 -0600

    Clean up files

---
+++ .spacemacs.d/layers/writing/funcs.el
@@ -0,0 +1,11 @@
+;;; funcs.el --- writing layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author:  <aidan@excli>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
