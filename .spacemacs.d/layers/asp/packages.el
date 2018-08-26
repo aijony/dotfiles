bdb8b070601780fdcb649d3825306cbcb6947b18
Aidan Nyquist <contact@aidannyquist.com>
2018-08-26 15:22:37 +0200

    Extend asp layer

--- .spacemacs.d/layers/asp/packages.el
+++ .spacemacs.d/layers/asp/packages.el
@@ -1,6 +1,9 @@
 
 
-(setq asp-packages '((asp-mode :location local)))
+(setq asp-packages '(
+                     (asp-mode :location local)
+                     (csharp-mode)
+                     ))
 
 (defun asp/init-asp-mode ()
   (use-package asp-mode
@@ -9,3 +12,22 @@
     :config
     (setq auto-mode-alist 
           (cons '("\\.asp\\'" . asp-mode) auto-mode-alist))))
+
+(defun asp/post-init-csharp-mode ()
+  (add-hook 'csharp-mode-hook 'ml-comments)
+  )
+
+;; (defun asp/post-init-mmm-mode ()
+;;   (use-package mmm-mode
+;;     :defer t
+;;     :commands mmm-mode
+;;     :config
+;;     (setq global-mmm-mode 'maybe)
+;;     (mmm-add-classes
+;;      '(
+;;        (csharp-xml
+;;         :submode xml-mode
+;;         :face mmm-code-submode-face
+;;         :front "\/\/\/"
+;;         )))
+;;     (mmm-add-mode-ext-class 'csharp-mode nil 'csharp-xml)))
